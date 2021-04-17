b04afe8982c3920f01b0a4bee072d862b29017d6
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 13:05:34 -0400

    started app

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
+    path('cubes/', views.cubes_index, name='index')
+]
---
+++ main_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+class Cube:
+    def __init__(self, name, brand, description):
+        self.name = name
+        self.brand = brand
+        self.description = description
+
+cubes = [
+    Cube('365 Air M', 'GAN', 'Very light cube with magnets')
+    Cube('ThunderClap', 'MoFangGe', 'Best speed cube for the price')
+    Cube('Pyraminx', 'MoYu', 'Best pyraminx on the market with magnets')
+]
+
+
+def home(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cubes_index(request):
+    return render(request, 'cubes/index.html', {'cubes': cubes})
