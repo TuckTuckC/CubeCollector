90a58c463d999852387726befc8bae8321313619
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 14:23:50 -0400

    removes the class cube and the cube list from views beacuse there is a database now

--- main_app/views.py
+++ main_app/views.py
@@ -3,19 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-class Cube:
-    def __init__(self, name, brand, description):
-        self.name = name
-        self.brand = brand
-        self.description = description
-
-cubes = [
-    Cube('365 Air M', 'GAN', 'Very light cube with magnets'),
-    Cube('ThunderClap', 'MoFangGe', 'Best speed cube for the price'),
-    Cube('Pyraminx', 'MoYu', 'Best pyraminx on the market with magnets'),
-]
-
-
 def home(request):
     return render(request, 'home.html')
 
