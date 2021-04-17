21bb67ade585efe098ab51036ae2e301783efea8
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 14:05:48 -0400

    added the cube model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
