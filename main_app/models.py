2730792e4ccffce614df1f5fd7390539d400488b
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 14:21:13 -0400

    cubes in the model now prints with the name

--- main_app/models.py
+++ main_app/models.py
@@ -4,3 +4,6 @@ class Cube(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
