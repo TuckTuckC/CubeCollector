3a575a01db155fe001e30237ee9b954297873bd5
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 15:05:16 -0400

    added a create route and updated the navbar in the bade.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cubes/', views.cubes_index, name='index'),
-    path('cubes/<int:cube_id>/', views.cube_detail, name='deatil')
+    path('cubes/<int:cube_id>/', views.cube_detail, name='deatil'),
+    path('cubes/create/', views.CubeCreate.as_view(), name='cubes_create'),
 ]
