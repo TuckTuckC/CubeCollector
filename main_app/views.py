8d359e613c4f408110fe31f999b9e751c570d2bd
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 17:30:00 -0400

    imported updateview and deleteview to views.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cube
 
 
