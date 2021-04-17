6290a726db67f8cca11416bde11973dd6565b409
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 14:45:47 -0400

    wrapped the cube card in an a tag

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cubes/', views.cubes_index, name='index'),
+    path('cubes/<int:cube_id>/', views.cube_deatils, name='deatils')
 ]
