2b13232b474a0a32943b9747f59b45f96c10d856
TuckTuckC <mrkittay847@gmail.com>
2021-04-17 17:27:30 -0400

    imported reverse in models and added update and delete paths

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 class Cube(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('cubes/', views.cubes_index, name='index'),
     path('cubes/<int:cube_id>/', views.cube_detail, name='deatil'),
     path('cubes/create/', views.CubeCreate.as_view(), name='cubes_create'),
+    path('cubes/<int:pk>/update/', views.CubeUpdate.as_view(), name='cubes_update'),
+    path('cubes/<int:pk>/delete/', views.CubeDelete.as_view(), name='cubes_delete'),
 ]
