3c399157c16c80ddf44f1cc918c3485767b3ab49
TuckTuckC <mrkittay847@gmail.com>
2021-04-26 23:50:59 -0400

    imported login and UserCreationForm in view.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cube, Stand, Photo
 from .forms import TimeForm
