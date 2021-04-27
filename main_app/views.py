7e44c497797ca28925ff55776701ccc0cd01b897
TuckTuckC <mrkittay847@gmail.com>
2021-04-26 23:54:35 -0400

    added a view function for signup

--- main_app/views.py
+++ main_app/views.py
@@ -112,3 +112,17 @@ def add_photo(request, cube_id):
         except:
             print('An error occurred uploading the file to S3')
     return redirect('detail', cube_id=cube_id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.us_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up, try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
